74e67c8e2e05f6444bbefaddf70ce572ce24e89c
juhom205 <80626044+juhom205@users.noreply.github.com>
2021-06-05 14:36:01 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,106 @@
+import random
+
+board = [i for i in range(0, 9)]
+player, computer = '', ''
+moves = ((1, 7, 3, 9), (5,), (2, 4, 6, 8))
+winners = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+tab = range(1, 10)
+
+
+def print_board():
+    x = 1
+    for i in board:
+        end = ' | '
+        if x % 3 == 0:
+            end = ' \n'
+            if i != 1: end += '---------\n';
+        char = ' '
+        if i in ('X', 'O'): char = i;
+        x += 1
+        print(char, end=end)
+
+
+def select_char():
+    chars = ('X', 'O')
+    if random.randint(0, 1) == 0:
+        return chars[::-1]
+    return chars
+
+
+def can_move(brd, move):
+    if move in tab and brd[move - 1] == move - 1:
+        return True
+    return False
+
+
+def can_win(brd, player):
+    places = []
+    x = 0
+    for i in brd:
+        if i == player: places.append(x);
+        x += 1
+    win = True
+    for tup in winners:
+        win = True
+        for ix in tup:
+            if brd[ix] != player:
+                win = False
+                break
+        if win:
+            break
+    return win
+
+
+def make_move(brd, player, move, undo=False):
+    if can_move(brd, move):
+        brd[move - 1] = player
+        win = can_win(brd, player)
+        if undo:
+            brd[move - 1] = move - 1
+        return True, win
+    return False, False
+
+
+def computer_move():
+    move = -1
+    for i in range(1, 10):
+        if make_move(board, computer, i, True)[1]:
+            move = i
+            break
+    if move == -1:
+        for i in range(1, 10):
+            if make_move(board, player, i, True)[1]:
+                move = i
+                break
+    if move == -1:
+        for tup in moves:
+            for mv in tup:
+                if move == -1 and can_move(board, mv):
+                    move = mv
+                    break
+    return make_move(board, computer, move)
+
+
+def space_exist():
+    return board.count('❌') + board.count('⭕') != 9
+
+
+player, computer = select_char()
+print('Player is [%s] and computer is [%s]' % (player, computer))
+result = '⭕ Drawn | You are on the same level as your computer'
+while space_exist():
+    print_board()
+    print('✅ Your turn | Choose a number between 1-9 to make a move ', end='')
+    move = int(input())
+    moved, won = make_move(board, player, move)
+    if not moved:
+        print(' ❌ Invalid number | Enter a number between 1-9')
+        continue
+    if won:
+        result = '✨ Congratulations | You are better than your computer'
+        break
+    elif computer_move()[1]:
+        result = '❌ You lost | Maybe next time?'
+        break
+print_board()
+print(result)
